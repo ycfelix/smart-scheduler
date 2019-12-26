2ab39799a7a2ca25661f0e0258aa695ecf7545ef
YCFELIX\yauch <ycfelix7@gmail.com>
2019-12-26 12:32:40 +0800

    done splash UI, done login UI

---
+++ SmartPH/app/src/main/java/com/ust/smartph/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.ust.smartph;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class LoginActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+        // Address the email and password field
+        emailEditText = findViewById(R.id.username);
+        passEditText = findViewById(R.id.password);
+
+    }
+
+    public void checkLogin(View arg0) {
+
+        final String email = emailEditText.getText().toString();
+        if (!isValidEmail(email)) {
+            //Set error message for email field
+            emailEditText.setError("Invalid Email");
+        }
+
+        final String pass = passEditText.getText().toString();
+        if (!isValidPassword(pass)) {
+            //Set error message for password field
+            passEditText.setError("Password cannot be empty");
+        }
+
+        if(isValidEmail(email) && isValidPassword(pass))
+        {
+            // Validation Completed
+        }
+
+    }
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() >= 4) {
+            return true;
+        }
+        return false;
+    }
+}
--- SmartPH/app/src/main/java/com/ust/smartph/SplashActivity.java
+++ SmartPH/app/src/main/java/com/ust/smartph/SplashActivity.java
@@ -7,15 +7,26 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        delay(3000);
-        startActivity(new Intent(SplashActivity.this,MainActivity.class));
-        // close splash activity
-
-        finish();
+        getSupportActionBar().hide();
+        setContentView(R.layout.splash_screen);
+        //delay thread
+        Thread th=new Thread(){
+            @Override
+            public void run() {
+                long begin=System.currentTimeMillis();
+                while (!canSwitch(begin,5000));
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        };
+        th.start();
     }
-    private void delay(int ms){
-        long begin=System.currentTimeMillis();
-        while(System.currentTimeMillis()-begin<ms);
+    private boolean canSwitch(long begin,int ms){
+        long now=System.currentTimeMillis();
+        if(now-begin>ms){
+            return true;
+        }
+        return false;
     }
 }
 
