e8f3d392a06efa6e5f43bed56a8d60649dda03cd
YCFELIX\yauch <ycfelix7@gmail.com>
2020-03-07 13:54:16 +0800

    Added app usage bar chart. Refactor needed. Timeline needed.

--- SmartPH/app/src/main/java/com/ust/appusagechart/BarchartFragment.java
+++ SmartPH/app/src/main/java/com/ust/appusagechart/BarchartFragment.java
@@ -10,7 +10,10 @@ import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -30,6 +33,8 @@ public class BarchartFragment extends Fragment {
     @BindView(R.id.usage_barchart)
     BarChart chart;
 
+    ArrayList<AppInfo> showList;
+
     private Unbinder unbinder;
     @Nullable
     @Override
@@ -37,6 +42,8 @@ public class BarchartFragment extends Fragment {
         ViewGroup root= (ViewGroup) inflater.inflate(R.layout.app_usage_barchart,container,false);
         unbinder= ButterKnife.bind(this,root);
         getActivity().setTitle("App Usage Bar Chart");
+        AppUsageInfo statisticsInfo = new AppUsageInfo(getActivity(),AppUsageInfo.DAY);
+        showList = statisticsInfo.getShowList();
         chart.getDescription().setEnabled(false);
         chart.getDescription().setEnabled(false);
 
@@ -55,7 +62,7 @@ public class BarchartFragment extends Fragment {
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(false);
-        xAxis.setLabelCount(10);
+        xAxis.setLabelCount(7);
         final ArrayList<String> xLabel = new ArrayList<>();
         for(int i=0;i<10;i++){
             xLabel.add(String.valueOf(i+10));
@@ -63,39 +70,94 @@ public class BarchartFragment extends Fragment {
         xAxis.setValueFormatter(new ValueFormatter() {
             @Override
             public String getAxisLabel(float value, AxisBase axis) {
-                return xLabel.get((int)value);
+                if(showList.size()<=(int)(value)) return "";
+                if((int)(value)>=6) return "others";
+                String name=showList.get((int)(value)).getLabel();
+                return name.length()<=4?name:name.substring(0,3)+"...";
             }
         });
 
-        chart.getAxisLeft().setDrawGridLines(false);
+        ValueFormatter custom=new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return (int) value + " min";
+            }
+        };
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(8, false);
+        rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
 
         // add a nice and smooth animation
         chart.animateY(1500);
 
-        chart.getLegend().setEnabled(false);
-        createDemoChart();
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setForm(Legend.LegendForm.SQUARE);
+        l.setFormSize(9f);
+        l.setTextSize(11f);
+        l.setXEntrySpace(4f);
+
+        MarkerView mv = new CustomMarkerView(getActivity());
+        mv.setChartView(chart); // For bounds control
+        chart.setMarker(mv); // Set the marker to the chart
+
+        setDataBarChart();
         return  root;
     }
-    private void createDemoChart(){
-        ArrayList<BarEntry> values = new ArrayList<>();
+    private void setDataBarChart() {
 
-        for (int i = 0; i < 10; i++) {
-            float multi = (i+ 1);
-            float val = (float) (Math.random() * multi) + multi / 3;
-
-            values.add(new BarEntry(i, val));
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+        if(showList.size() < 6) {
+            for (int i = 0; i < showList.size(); i++) {
+                float value = (float)(1.0 * showList.get(i).getUsedTimebyDay() / 1000 / 60);
+                yVals1.add(new BarEntry(i, value));
+            }
         }
+        else {
+            for(int i = 0;i < 6;i++) {
+                float value = (float)(1.0 * showList.get(i).getUsedTimebyDay() / 1000 / 60 );
+                yVals1.add(new BarEntry(i, value));
+            }
+            long otherTime = 0;
+            for(int i=6;i<showList.size();i++) {
+                otherTime += showList.get(i).getUsedTimebyDay();
+            }
+            yVals1.add(new BarEntry(6,(float)(1.0 * otherTime / 1000 / 60)));
+        }
+        BarDataSet set1;
 
-        BarDataSet set1 = new BarDataSet(values, "Data Set");
-        set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
-        set1.setDrawValues(false);
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals1);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "Different APPs");
+            set1.setColors(ColorTemplate.MATERIAL_COLORS);
 
-        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
 
-        BarData data = new BarData(dataSets);
-        chart.setData(data);
-        chart.setFitBars(true);
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(0.9f);
+
+            chart.setData(data);
+        }
         chart.invalidate();
     }
     @Override
